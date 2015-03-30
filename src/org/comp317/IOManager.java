0a3b8b730f1a379740775db1ac5bb4fb38df4b2c
jeffmomo <jeff941027@gmail.com>
2015-03-30 13:52:21 +1300

    Update IOManager.java

--- src/org/comp317/IOManager.java
+++ src/org/comp317/IOManager.java
@@ -32,17 +32,17 @@ public class IOManager
 	public BufferedWriter createOutputStreamWriter(String fname) throws Exception
 	{
 		if(_type == 0)
-			return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(_tempDirectory + fname), _charset), _writeBufferSize);
+			return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(_tempDirectory + File.separator + fname), _charset), _writeBufferSize);
 		else
-			return new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(_tempDirectory + fname), _writeBufferSize, false), _charset), _writeBufferSize);
+			return new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(_tempDirectory + File.separator + fname), _writeBufferSize, false), _charset), _writeBufferSize);
 	}
 
 	public BufferedReader createBufferedReader(String fname) throws Exception
 	{
                 if(_type == 0)
-			return new BufferedReader(new InputStreamReader(new FileInputStream(_tempDirectory + fname), _charset), _readBufferSize);
+			return new BufferedReader(new InputStreamReader(new FileInputStream(_tempDirectory + File.separator + fname), _charset), _readBufferSize);
 		else
-			return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(_tempDirectory + fname), _readBufferSize), _charset), _readBufferSize);
+			return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(_tempDirectory + File.separator + fname), _readBufferSize), _charset), _readBufferSize);
 	}
 
 	public void write(BufferedWriter writer, String item) throws IOException
@@ -54,7 +54,7 @@ public class IOManager
 
 	public File getFile(String fname)
 	{
-		return new File(_tempDirectory + fname);
+		return new File(_tempDirectory + File.separator + fname);
 	}
 
 	public int getType()
@@ -63,7 +63,7 @@ public class IOManager
 	}
 	public String getDirectory()
 	{
-		return _tempDirectory;
+		return _tempDirectory + File.separator;
 	}
 
 }
